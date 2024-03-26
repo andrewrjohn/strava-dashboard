c925a3060496ded0808a0c9f57f0f55b44ac7d1f
Andrew Johnson <johnsonrobertandrew@gmail.com>
2024-03-26 12:08:59 -0500

    minor

--- src/app/lib/numbers.ts
+++ src/app/lib/numbers.ts
@@ -6,7 +6,7 @@ export function formatTime(seconds: number) {
   const hours = Math.floor(seconds / 3600);
   const minutes = Math.floor((seconds % 3600) / 60);
   const secondsLeft = Math.floor(seconds % 60);
-  return `${hours ? `${hours}:` : ""}${minutes}:${secondsLeft
+  return `${hours ? `${hours}:` : ""}${minutes
     .toString()
-    .padEnd(2, "0")}`;
+    .padStart(2, hours ? "0" : "")}:${secondsLeft.toString().padEnd(2, "0")}`;
 }
