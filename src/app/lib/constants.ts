4a9a05a20648f049dd21c9d52c98cb0eea516302
Andrew Johnson <johnsonrobertandrew@gmail.com>
2024-03-29 08:14:59 -0500

    use production domain for redirect

--- src/app/lib/constants.ts
+++ src/app/lib/constants.ts
@@ -1,7 +1,12 @@
-export const STRAVA_APP_ID = 122669;
+export const STRAVA_APP_ID = 122669
 
-export const STRAVA_AUTHORIZATION_URL = `https://www.strava.com/oauth/authorize?client_id=${STRAVA_APP_ID}&response_type=code&redirect_uri=http://localhost:3000/login&approval_prompt=force&scope=activity:read_all,profile:read_all,read_all`;
+const redirectBaseUrl =
+  process.env.NODE_ENV === 'production'
+    ? 'https://run.weaklytyped.com'
+    : 'http://localhost:3000'
+
+export const STRAVA_AUTHORIZATION_URL = `https://www.strava.com/oauth/authorize?client_id=${STRAVA_APP_ID}&response_type=code&redirect_uri=${redirectBaseUrl}/login&approval_prompt=force&scope=activity:read_all,profile:read_all,read_all`
 
 export const COOKIES = {
-  STRAVA_ATHLETE_ID: "strava_athlete_id",
-};
+  STRAVA_ATHLETE_ID: 'strava_athlete_id',
+}
