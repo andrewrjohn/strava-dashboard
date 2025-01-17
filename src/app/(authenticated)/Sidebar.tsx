5f0b013dc7e4f450ec04af79f465ea7f21ad4c64
Andrew Johnson <johnsonrobertandrew@gmail.com>
2025-01-17 12:39:58 -0600

    fix screen height issue mobile

---
+++ src/app/(authenticated)/Sidebar.tsx
@@ -0,0 +1,114 @@
+'use client'
+
+import LogoutButton from '@/components/LogoutButton'
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
+import { Button } from '@/components/ui/button'
+import { cn } from '@/lib/utils'
+import { DetailedAthlete } from '@/types/interfaces'
+import {
+  BarChartIcon,
+  CalendarIcon,
+  FootprintsIcon,
+  HomeIcon,
+} from 'lucide-react'
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+import { useState } from 'react'
+import { useEffect } from 'react'
+
+const routes = [
+  {
+    label: 'Overview',
+    icon: HomeIcon,
+    href: '/',
+  },
+  {
+    label: 'Training Log',
+    icon: CalendarIcon,
+    href: '/training-log',
+  },
+  {
+    label: 'Stats',
+    icon: BarChartIcon,
+    href: '/stats',
+  },
+  {
+    label: 'Gear',
+    icon: FootprintsIcon,
+    href: '/gear',
+  },
+]
+
+interface Props {
+  athlete: DetailedAthlete
+}
+
+export function Sidebar(props: Props) {
+  const { athlete } = props
+
+  const pathname = usePathname()
+
+  const [windowHeight, setWindowHeight] = useState(0)
+
+  useEffect(() => {
+    const updateHeight = () => {
+      setWindowHeight(window.innerHeight)
+    }
+
+    updateHeight()
+
+    window.addEventListener('resize', updateHeight)
+
+    return () => {
+      window.removeEventListener('resize', updateHeight)
+    }
+  }, [])
+
+  return (
+    <div
+      // Fallback to CSS 'h-screen' so we minimal UI jump while the
+      // windowHeight is being calculated
+      className="flex h-screen flex-col border-r bg-muted/10"
+      style={{ height: windowHeight || undefined }}
+    >
+      <div className="flex h-14 items-center border-b px-6">
+        <h1 className="text-xl font-bold sr-only md:not-sr-only">Run Hub</h1>
+        <h1 className="text-xl font-bold md:hidden block">RH</h1>
+      </div>
+      <div className="flex-1 space-y-1 p-3">
+        {routes.map(({ label, href, icon: Icon }) => (
+          <Button
+            key={href}
+            variant={pathname === href ? 'secondary' : 'ghost'}
+            className={cn(
+              'md:w-full flex justify-center md:justify-start md:gap-2',
+            )}
+            asChild
+          >
+            <Link href={href}>
+              <Icon className="h-4 w-4" />
+              <span className="hidden md:inline">{label}</span>
+            </Link>
+          </Button>
+        ))}
+      </div>
+      <div className="border-t p-4">
+        <div className="flex flex-col md:flex-row items-center gap-3">
+          <Avatar>
+            <AvatarImage src={athlete.profile} />
+            <AvatarFallback>
+              {athlete.firstname.charAt(0)}
+              {athlete.lastname.charAt(0)}
+            </AvatarFallback>
+          </Avatar>
+          <div className="flex flex-col gap-2">
+            <span className="text-sm font-medium hidden md:block">
+              {athlete.firstname} {athlete.lastname}
+            </span>
+            <LogoutButton />
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
--- src/app/(authenticated)/layout.tsx
+++ src/app/(authenticated)/layout.tsx
@@ -1,12 +1,8 @@
-import { NavLinks } from '@/components/NavLinks'
-import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
-import PoweredByStrava from '@/images/api_logo_pwrdBy_strava_horiz_gray.svg'
+import { FooterImage } from '@/components/FooterImage'
 import { getAthleteId } from '@/lib/cookies'
-import Image from 'next/image'
 import { redirect } from 'next/navigation'
 import { getAthlete } from '../actions'
-import LogoutButton from '@/components/LogoutButton'
-import { FooterImage } from '@/components/FooterImage'
+import { Sidebar } from './Sidebar'
 
 export default async function RootLayout({
   children,
@@ -23,30 +19,7 @@ export default async function RootLayout({
 
   return (
     <div className="flex h-screen overflow-hidden">
-      <div className="flex h-screen flex-col border-r bg-muted/10">
-        <div className="flex h-14 items-center border-b px-6">
-          <h1 className="text-xl font-bold sr-only md:not-sr-only">Run Hub</h1>
-          <h1 className="text-xl font-bold md:hidden block">RH</h1>
-        </div>
-        <NavLinks />
-        <div className="border-t p-4">
-          <div className="flex flex-col md:flex-row items-center gap-3">
-            <Avatar>
-              <AvatarImage src={athlete.profile} />
-              <AvatarFallback>
-                {athlete.firstname.charAt(0)}
-                {athlete.lastname.charAt(0)}
-              </AvatarFallback>
-            </Avatar>
-            <div className="flex flex-col gap-2">
-              <span className="text-sm font-medium hidden md:block">
-                {athlete.firstname} {athlete.lastname}
-              </span>
-              <LogoutButton />
-            </div>
-          </div>
-        </div>
-      </div>
+      <Sidebar athlete={athlete} />
 
       <div className="md:px-7 px-4 py-3 md:py-6 flex flex-col flex-1 overflow-y-scroll w-full">
         <div className="flex-1 flex flex-col">{children}</div>
